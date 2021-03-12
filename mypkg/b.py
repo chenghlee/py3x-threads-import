cd613b8e0ca4164e95ad6656496ba2fc1e584ccf
Cheng H. Lee <clee@anaconda.com>
2021-03-12 10:19:16 -0600

    Shorten delay when import `mypkg.b`
    Far easier to trigger the behavior by increasing the number of threads
    (via the `-t` CLI argument) than modifying the `import` time.

--- mypkg/b.py
+++ mypkg/b.py
@@ -2,9 +2,9 @@ from time import sleep
 from threading import current_thread
 
 # For the buggy(?) behavior to manifest itself, this sleep should be
-# significantly (i.e., several orders of magnitude) longer than the thread
-# switching interval set using `sys.setswitchinterval()`.
-sleep(2.50)
+# significantly longer than the thread switching interval set using
+# `sys.setswitchinterval()`.
+sleep(0.25)
 
 def fn():
     thread = current_thread()
